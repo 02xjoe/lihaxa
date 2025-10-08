f578dc2cf1d967c40c095a19cc5fda49c6b6beda
02xjoe <johntobif@gmail.com>
2025-10-08 14:16:06 +0100

    ll

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
- base: "/lihaxa/",
+ // base: "/lihaxa/",
   plugins: [react(), tailwindcss()],
 })
 

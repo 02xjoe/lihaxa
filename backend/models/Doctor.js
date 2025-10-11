fd996bf9506dc3e16df102fc0040eda6c57ea783
02xjoe <johntobif@gmail.com>
2025-10-11 14:05:02 +0100

    11/10

--- backend/models/Doctor.js
+++ backend/models/Doctor.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+const result = await sendMailToUserAndCompany(formData);
+
 const doctorSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
